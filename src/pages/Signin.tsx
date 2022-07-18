95625894cf0f6d3a02b348353ca565f388269beb
Mohammad Arfan <32129450+marfanr@users.noreply.github.com>
2022-07-18 22:34:14 +0700

    update signin page

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -8,21 +8,21 @@ function Signin() {
     return (
         <div className="container mx-auto xl:w-5/12 lg:w-1/2 ">
                 <div className="bg-white p-6 lg:shadow lg:mt-14 lg:border lg:border-grey-900">                                        
-                        <div className="lg:mb-9 mb-16">
+                        <div className="lg:mb-9 mb-5">
                             <h4 className="text-md font-bold text-2xl">Sign-In</h4>
                             <span className="text-sm">Sign-in to <span className="text-purple-700 font-bold">tieldmon</span> before starting...</span>
                         </div>                    
                         <div className="lg:mx-3">                        
                             {/* <form> */}
-                                <div className="mb-3">
-                                    <label htmlFor="email" className="mb-1">email</label>
+                                <div className="mb-4">
+                                    <label htmlFor="email" className="mb-2">email</label>
                                     <input type="email" id="email" className="border rounded w-full p-4" name="email"  placeholder="Email"/>
                                 </div>
-                                <div className="mb-3">
-                                    <label className="mb-1">password</label>
-                                    <input type="password" className="border rounded w-full p-4" name="" placeholder="Password" />
+                                <div className="mb-4">
+                                    <label htmlFor="password" className="mb-2">password</label>
+                                    <input id="password" type="password" className="border rounded w-full p-4" name="" placeholder="Password" />
                                 </div>
-                                <div className="mb-3 flex  items-center">
+                                <div className="mb-6 flex  items-center">
                                     <input type="checkbox" name="" id="" className="flex-none mr-2 border"/>
                                     <span className="text-md flex-grow">Remember me ?</span>
                                 </div>
