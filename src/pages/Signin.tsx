c58db44e334f841924767bea8cd58621b1d3db8b
Mohammad Arfan <32129450+marfanr@users.noreply.github.com>
2022-07-18 14:17:00 +0700

    update progress

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,11 +1,22 @@
 import { useEffect } from "react"
+import "../styles/signin.css"
 
 function Signin() {
     useEffect(() => {
         document.title = 'Tieldmoon | Sign-in';
     })
     return (
-        <div>s</div>
+        <div className="container mx-auto w-5/12">            
+                <div className="mt-9 bg-white p-5">                    
+                    <div className="mb-8">
+                        <h4 className="text-md font-bold text-xl">Sign-In</h4>
+                        <span className="text-sm">Sign-in to <span className="text-purple-700 font-bold">tieldmon</span> before starting...</span>
+                    </div>                    
+
+                    s
+                </div>
+            
+        </div>
     )
 }
 
