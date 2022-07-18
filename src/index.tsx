6a68b11d3086c99c4327cffe61cb67681e5ca227
Mohammad Arfan <32129450+marfanr@users.noreply.github.com>
2022-07-18 12:04:48 +0700

    setup reactjs + tailwind + react router

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Signin from './pages/Signin';
 import reportWebVitals from './reportWebVitals';
+import {HashRouter, Route, Routes} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/signin" element={<Signin />} />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 );
 
---
+++ src/pages/Signin.tsx
@@ -0,0 +1,12 @@
+import { useEffect } from "react"
+
+function Signin() {
+    useEffect(() => {
+        document.title = 'Tieldmoon | Sign-in';
+    })
+    return (
+        <div>s</div>
+    )
+}
+
+export default Signin
